2184ab3e148aa25092a2925419e97433e6821e3e
AlondraDelmar <alondradelmar999@gmail.com>
2023-12-03 14:36:47 -0600

    new file.

--- data/11.2.23.R
+++ data/11.2.23.R
@@ -21,19 +21,83 @@ library(gmodels)
 library(export)
 library(tidyverse)
 #Calculate the Principal Components (PC) using fast.prcomp included in gmodels.
-pca.info <- fast.prcomp(Brown_adipose_2022)
+dat_pca = Brown_adipose_2022[,2:49]
+pca.info = fast.prcomp(dat_pca)
 #check PCA results.
 head(pca.info)
 summary(pca.info) #You can see the specific explained variance for each PC.
 head(pca.info$rotation) #regression coefficients
 head(pca.info$sdev) #square root of eigenvalue
 head(pca.info$x) #sample score
-pca.data <- data.frame(sample = rownames(pca.info$rotation),Type=c(rep("4_IBA",8),rep("5_MAR",8),rep("2_OCT",9), rep("1_SEP",9),rep("3_TOR",14)), pca.info$rotation)
+pca.data = data.frame(sample = rownames(pca.info$rotation),Type=c(rep("4_IBA",8),rep("5_MAR",8),rep("2_OCT",9), rep("1_SEP",9),rep("3_TOR",14)), pca.info$rotation)
 #according to the name you generate when read your excel
 m<-colnames(Brown_adipose_2022)[1:48]
+#first version of pca plot
 ggscatter(pca.data,x="PC1", y="PC2", color="Type",
           size=1, ellipse.border.remove=TRUE,
-          label=c(m), repel=TRUE, main="PCA plot"+theme_base())
+          , repel=TRUE, main="PCA plot"+theme_base())
+#second version of pca plot
+dat2=t(Brown_adipose_2022[,2:49])
+dat2=as.data.frame(dat2)
+Type=c(rep("4_IBA",8),rep("5_MAR",8),rep("2_OCT",9), rep("1_SEP",9),rep("3_TOR",14))
+dat2$Type=Type
+length(dat2[1,])
+ord=prcomp(dat2[,1:356])
+summary(ord)
+dt=ord$x
+df=data.frame(dt,dat2$Type)
+head(df)
+summ<-summary(ord)
+xlab=paste0("PC1(",round(summ$importance[2,1]*100,2),"%)")
+ylab=paste0("PC2(",round(summ$importance[2,2]*100,2),"%)")
+ggplot(df,aes(df$PC1,df$PC2,color=df$dat2.Type))+
+  stat_ellipse(aes(fill=df$dat2.Type),type="norm",geom="polygon",alpha=0.2,color=NA)+
+  guides(fill="none")+
+  geom_point()+
+  labs(x=xlab,y=ylab,color="")
+
+
+
+
+
+# Assuming Brown_adipose_2022 is your data frame
+dat_pca <- Brown_adipose_2022[, 2:49]
+pca.info <- fast.prcomp(dat_pca)
+
+# Create a data frame for plotting
+pca.data <- data.frame(sample = rownames(pca.info$rotation),
+                       Type = c(rep("4_IBA", 8), rep("5_MAR", 8), rep("2_OCT", 9), rep("1_SEP", 9), rep("3_TOR", 14)),
+                       pca.info$rotation)
+
+# Extract column names for labeling
+m <- colnames(Brown_adipose_2022)[1:48]
+
+# PCA plot using ggscatter
+p <- ggscatter(pca.data, x = "PC1", y = "PC2", color = "Type",
+               size = 1, ellipse.border.remove = TRUE,
+               label = c(m), repel = TRUE, main = "PCA plot" + theme_base())
+
+# Add circles around points for each variable
+for (variable in m) {
+  data_subset <- pca.data[, c("PC1", "PC2", "Type", variable)]
+  ellipse_params <- ellipse(cor(data_subset[, c("PC1", "PC2")]),
+                            center = colMeans(data_subset[, c("PC1", "PC2")]),
+                            level = 0.85)
+
+  p <- p + geom_point(data = ellipse_params, aes(x = "PC1", y = "PC2"),
+                      color = scales::hue_pal()(1)[as.numeric(factor(data_subset$Type[1]))],
+                      size = 1.5, alpha = 0.2)
+}
+
+# Print the PCA plot
+print(p)
+
+
+
+
+
+
+
 #next objectives is too: customize plot and create heatmmap
 #install.packages("ggplot2")
 library(ggplot2)
